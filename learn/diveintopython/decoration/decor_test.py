84393cc587a7c5681f502359493732f22d89b897
zhenghaitao <zhtsuc@gmail.com>
2017-06-06 15:40:05 +0800

    check in

---
+++ learn/diveintopython/decoration/decor_test.py
@@ -0,0 +1,46 @@
+
+
+def log(func):
+    def wrapper(*args, **kw):
+        print 'call %s():' % func.__name__
+        return func(*args, **kw)
+    return wrapper
+
+@log
+def now():
+    print '2013-12-25'
+
+
+now()
+
+def log1(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print '%s %s():' % (text, func.__name__)
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@log1('execute')
+def now1():
+    print '2013-12-25'
+
+now1()
+
+print now1.__name__
+
+import functools
+
+def log2(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print 'call %s():' % func.__name__
+        return func(*args, **kw)
+    return wrapper
+
+@log2
+def now2():
+    print '2013-12-25'
+
+
+print now2.__name__
